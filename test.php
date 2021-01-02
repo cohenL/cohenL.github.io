fe69fc7fc84ee40225f12448ff1202cab081501e
cohenL <56096934+cohenL@users.noreply.github.com>
2021-01-02 13:52:34 -0500

    php test

---
+++ test.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cohen Ly</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./files/css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<!-- navbar -->
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	  <a class="navbar-brand ml-1" href="index.html"><img id="nav-logo" src="./files/images/nav_logo.png"> Cohen Nicholls Ly</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+	    <ul class="navbar-nav ml-auto mr-1">
+	      <li class="nav-item">
+	        <a class="nav-link" href="index.html">Home</a>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="comtech.html" id="navbarDropdownMenuLink" role="button"  aria-haspopup="true" aria-expanded="false">
+	          Comtech
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="wordDesign.html">Word Desigh</a>
+	          <a class="dropdown-item" href="typeDesign.html">Type Design</a>
+	          <a class="dropdown-item" href="selfPortrait.html">Self Portrait</a>
+	          <a class="dropdown-item" href="soundtrack.html">Soundtrack</a>
+	          <a class="dropdown-item" href="productionLogo.html">Production Logo</a>
+	          <a class="dropdown-item" href="websiteDesign.html">Website Design</a>
+	          <a class="dropdown-item" href="composite.html">Composite</a>
+
+	        </div>
+	      </li>
+   		  <li class="nav-item">
+	        <a class="nav-link active" href="co-op.html">Co-Op</a>	       
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="programming.html" id="navbarDropdownMenuLink" role="button"  aria-haspopup="true" aria-expanded="false">
+	          Programming
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="schoolTrivia.html">School Trivia</a>
+	          <a class="dropdown-item" href="factorBetting.html">Factor Betting</a>
+	          <a class="dropdown-item" href="myMusic.html">My Music</a>
+	          <a class="dropdown-item" href="finalProject.html">Final Project</a>
+	        </div>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="aboutMe.html">About Me</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="contactMe.html">Contact Me</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	<!-- navbar -->
+
+
+	<!-- jumbotron -->
+	<div class="jumbotron jumbotron-fluid text-light jumbotron-coop mb-0 py-4">
+		<div class="container">
+		  <h1 class="display-3">PHP Test</h1>
+		</div>
+	</div>
+	<!-- jumbotron -->
+
+
+  	<!-- LeadFeast 1 -->
+		<div class="bg-light text-dark p-3">
+			<div class="container">
+				<div class="row align-items-center">
+	        <div class="col-md-8 order-2 order-md-1">
+            <div id="headingGroup" class="py-3">
+                <h2 class="project-phase py-2">LeadFeast Initial Designs</h2>
+                <p class="">
+                	The first project I was given druing my co-op was to create a logo for a company called LeadFeast. I was given the instructions to make it look high tech and the rest I had freedom of choice. I really enjoyed this task becaue it gave me a lot of free range of what I wanted the design to look like and let me creatively think and make my own designs. I created many designs and ran it through my supervisers to determine which would look best for the company.
+                </p> 
+            </div>
+        	</div>
+        	<div class="col-md-4 order-1 order-md-2">
+            <img class="img-fluid" src="files/images/leadFeastLogos.png" alt="">
+        	</div>
+	    	</div>
+			</div>
+		</div>
+		<!-- LeadFeast 1 -->
+
+		<!-- LeadFeast 2 -->
+		<div class="bg-dark text-light p-3">
+			<div class="container">
+				<div class="row align-items-center">
+	        <div class="col-md-8 order-2 order-md-2">
+            <div id="headingGroup" class="py-3">
+                <h2 class="project-phase py-2">LeadFeast Final Design</h2>
+                <p class="">
+                	After we determined the base design of the logo I was given the task to create different crations of the logo with different colours for the letters aswell as have different shapes around the logo. I created many different variations of the logo, but my supervisors went with the blue colors and a circle around the logo. I did final adjustments to the design such as making the colour of the full name a lighter blue to stand out more and was then finished with the task.
+                </p>
+            </div>
+        	</div>
+        	<div class="col-md-4 order-1 order-md-1">
+            <img class="img-fluid" src="files/images/leadFeastFinal.jpg" alt="">
+        	</div>
+	    	</div>
+			</div>
+		</div>
+		<!-- LeadFeast 2 -->
+
+		<!-- Genesis Labs 1 -->
+		<div class="bg-light text-dark p-3">
+			<div class="container">
+				<div class="row align-items-center">
+	        <div class="col-md-8 order-2 order-md-1">
+            <div id="headingGroup" class="py-3">
+                <h2 class="project-phase py-2">Genesis Labs Initial Designs</h2>
+                <p class="">
+                	The next project I was given I found it much more difficult to create a design. I was given the task to create a logo for the company Genesis Labs, I was told to have some sort of tech or engineering in the design. I found it  difficult to create a good design that I like and thought would be appropiate for the company. I eventually came up with a design, but I did not like the way it looked. I took it up with my supervisor and he told me to talk to a designe who was a designer at Carleton Univeristy. She suggested I read some articles and books as well as watch a doccumentary on becoming more creative. 
+                </p> 
+            </div>
+        	</div>
+        	<div class="col-md-4 order-1 order-md-2">
+            <img class="img-fluid" src="files/images/genesisLabsInitial.jpg" alt="">
+        	</div>
+	    	</div>
+			</div>
+		</div>
+		<!-- Genesis Labs 1 -->
+
+		<!-- Genesis Labs 2 -->
+		<div class="bg-dark text-light p-3">
+			<div class="container">
+				<div class="row align-items-center">
+	        <div class="col-md-8 order-2 order-md-2">
+            <div id="headingGroup" class="py-3">
+                <h2 class="project-phase py-2">Genesis Labs Final Design</h2>
+                <p class="">
+                	After I took a break from designing and learned how to creatively think, I designed a initial design but did not like the colours. I asked what my supervisor thought and he liked the overall designer except for a few small adgustments and suggested I try a bunch of colour combinations and see what jumps out to me. I tried out many different colour combinations and liked a few of them but the design in the bottom right was what I thought looked the best. I asked what my supervior thought and he really liked the designh as well.
+                </p>
+            </div>
+        	</div>
+        	<div class="col-md-4 order-1 order-md-1">
+            <img class="img-fluid" src="files/images/genesisLabsLogos.png" alt="">
+        	</div>
+	    	</div>
+			</div>
+		</div>
+		<!-- Genesis Labs 2 -->	
+
+		<!-- Genesis Labs 3 -->
+		<div class="bg-light text-dark p-3">
+			<div class="container">
+				<div class="row align-items-center">
+	        <div class="col-md-8 order-2 order-md-1">
+            <div id="headingGroup" class="py-3">
+                <h2 class="project-phase py-2">Genesis Labs Holiday Designs</h2>
+                <p class="">
+                	After creating the logo for Genesis Labs I was given the task to modify the logo for the holidays over the year. I really enjoyed this task because it gave me a lot of creative freedom. I could put what I wanted in each design for each of the holidays. This was a really fun and enjoyable project to complete.
+                </p> 
+            </div>
+        	</div>
+        	<div class="col-md-4 order-1 order-md-2">
+            <img class="img-fluid" src="files/images/genesisLabsHolidayLogos.png" alt="">
+        	</div>
+	    	</div>
+			</div>
+		</div>
+		<!-- Genesis Labs 3 -->
+
+
+	<!-- javascript -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="./files/js/script.js"></script>
+</body>
